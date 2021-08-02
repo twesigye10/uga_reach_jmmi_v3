a091f916043e76fff59b3c3f84fcc464a11a2df4
Anthony Twesigye <twesigye10@gmail.com>
2021-08-02 21:25:19 +0300

    Added market monitoring script

---
+++ R/market_monitoring_analysis_script.R
@@ -0,0 +1,16 @@
+# Uganda Market Monitoring R Script - V3
+
+today <- Sys.Date()
+
+# Download and install hypegrammaR from IMPACT GitHub
+# devtools::install_github("impact-initiatives/hypegrammaR", build_opts = c())
+
+# Load required packaged
+library(tidyverse)
+library(readxl)
+library(data.table)
+library(hypegrammaR)
+library(lubridate)
+library(butteR)
+
+
