a82ed0623ea83844d9f1edc9738ee9a7844de9cf
Anthony Twesigye <twesigye10@gmail.com>
2021-08-03 09:20:41 +0300

    turned the script into a function

--- R/location_list.R
+++ R/location_list.R
@@ -1,25 +1,24 @@
-# location data -----------------------------------------------------------
+# process location data (settlements and districts)
 
-# Load locations data
-settlement_data <- read_csv("inputs/settlement_list.csv", na = c(""," ", "NA"))
-district_data <- read_csv("inputs/Districts_list.csv", na = c(""," ","NA"))
-
-# settlement data with settlement names in lower case
-settlement_data <- settlement_data %>% 
-  rename(settlement = NAME0) %>% 
-  mutate(settlement = str_to_lower(settlement))
-
-# district data with district names in lower case
-district_data <- district_data %>%  mutate(district = str_to_lower(DName2019))
-
-# Aggregate coordinates for Adjumani and add the aggregated value to the settlements coordinates list
-adjumani_coordinates <- settlement_data %>% 
-  filter(DISTRICT == "adjumani") %>% 
-  select(DISTRICT,Longitude,Latitude) %>% 
-  group_by(DISTRICT) %>%
-  summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
-  mutate(settlement = "adjumani")
-
-settlement_data <- bind_rows(settlement_data, adjumani_coordinates)  
-
-rm(Adjumani_coordinates)
+settlement_district <- function(input_setlement, input_districts) {
+  # settlement data with settlement names in lower case
+  settlement_data <- input_setlement %>% 
+    rename(settlement = NAME0) %>% 
+    mutate(settlement = str_to_lower(settlement))
+  
+  # district data with district names in lower case
+  district_data <- input_districts %>%  
+    mutate(district = str_to_lower(DName2019))
+  
+  # Aggregate coordinates for Adjumani and add the aggregated value to the settlements coordinates list
+  adjumani_coordinates <- settlement_data %>% 
+    filter(DISTRICT == "adjumani") %>% 
+    select(DISTRICT,Longitude,Latitude) %>% 
+    group_by(DISTRICT) %>%
+    summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
+    mutate(settlement = "adjumani")
+  
+  settlement_data <- bind_rows(settlement_data, adjumani_coordinates)  
+  
+  settlement_district_data <- list(settlements = settlement_data, districts = district_data)
+}
