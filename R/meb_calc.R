8ba090f61aba23967c5a5c562ee1b5198565268e
Anthony Twesigye <twesigye10@gmail.com>
2021-08-05 08:29:29 +0300

    imputation based on a previous round

--- R/meb_calc.R
+++ R/meb_calc.R
@@ -16,8 +16,8 @@ meb_items <- item_prices %>%
 
 
 # Calculate proximity: if a price is missing take the mean of settlement, or district, otherwise, regions
-# imputation of NAs
 
+# imputation of NAs based on means of the current month(yrmo)
 meb_items <- meb_items %>% 
   group_by(settlement, yrmo) %>% 
   mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
@@ -28,3 +28,38 @@ meb_items <- meb_items %>%
   group_by(regions, yrmo) %>% 
   mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) 
 
+prior_2_rounds <- c(yrmo_to_include[length(yrmo_to_include) - 1], yrmo_to_include[length(yrmo_to_include) - 2])
+current_and_prior_round <- c(yrmo_to_include[length(yrmo_to_include)], yrmo_to_include[length(yrmo_to_include) - 1])
+
+# if NA, put price from last round
+meb_items_for_this_month_imputation <- meb_items %>% 
+  filter(yrmo %in% current_and_prior_round)
+
+meb_items_for_prev_month_imputation <- meb_items %>% 
+  filter(yrmo %in% prior_2_rounds)
+
+# this improvement should yield better values since we start by aggregating at settlement level
+meb_items_this_round <- meb_items_for_this_month_imputation %>% 
+  group_by(settlement) %>% 
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  ungroup() %>% 
+  group_by(district) %>% 
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  ungroup() %>% 
+  group_by(regions) %>% 
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  filter(yrmo %in% yrmo_constructed)
+
+meb_items_last_round <- meb_items_for_prev_month_imputation %>% 
+  group_by(settlement) %>% 
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  ungroup() %>% 
+  group_by(district) %>% 
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  ungroup() %>% 
+  group_by(regions) %>% 
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) %>% 
+  filter(yrmo %in% prior_2_rounds[1])
+
+# meb items for this and last rounds
+meb_items <- bind_rows(meb_items_this_round, meb_items_last_round)
