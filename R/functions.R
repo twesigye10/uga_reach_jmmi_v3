1295a4527bf4a2abccb52f1f3fcd51f796a854b7
Anthony Twesigye <twesigye10@gmail.com>
2021-08-30 08:29:38 +0300

    moved location processing and region regrouping functions to the functions script

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,44 @@
+# process location data (settlements and districts)
+
+settlement_district <- function(input_setlement, input_districts) {
+  # settlement data with settlement names in lower case
+  settlement_data <- input_setlement %>% 
+    rename(settlement = NAME0) %>% 
+    mutate(settlement = str_to_lower(settlement))
+  
+  # district data with district names in lower case
+  district_data <- input_districts %>%  
+    mutate(district = str_to_lower(DName2019))
+  
+  # Aggregate coordinates for Adjumani and add the aggregated value to the settlements coordinates list
+  adjumani_coordinates <- settlement_data %>% 
+    filter(DISTRICT == "adjumani") %>% 
+    select(DISTRICT,Longitude,Latitude) %>% 
+    group_by(DISTRICT) %>%
+    summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
+    mutate(settlement = "adjumani")
+  
+  settlement_data <- bind_rows(settlement_data, adjumani_coordinates) %>% 
+    janitor::clean_names()
+  
+  settlement_district_data <- list(settlements = settlement_data, districts = district_data)
+}
+
+# regroup regions
+regroup_regions<-function(input_df){
+  south_west<-c("kyaka", "kyangwali", "nakivale", "oruchinga", "rwamwanja")
+  west_nile<- c("adjumani", "rhino camp", "bidibidi", "imvepi", "kiryandongo", 
+                "lobule", "palabek", "palorinya")
+  input_df %>% 
+    mutate(
+      regions = case_when(
+        yrmo > 202012 & settlement %in% south_west ~ "south west",
+        yrmo > 202012 & settlement %in% west_nile ~ "west nile",
+        TRUE ~ regions
+      )
+    )
+}
+
 # get files names and full paths into a list
 get_files_metadata<-function(folder_path){
   fps<-list()
