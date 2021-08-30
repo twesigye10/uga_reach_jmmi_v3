28ab84c37c26c6fe021dde585daae9f810bcf1e7
Anthony Twesigye <twesigye10@gmail.com>
2021-08-30 08:33:42 +0300

    added the month_specific_cleaning function to the functions.R script

--- R/functions.R
+++ R/functions.R
@@ -39,6 +39,20 @@ regroup_regions<-function(input_df){
     )
 }
 
+# specific data cleaning for nov 2020 - round 11 (in case of re calculating past values)
+
+month_specific_cleaning<-function(input_df){
+  
+  input_df %>% 
+    mutate(vendors_change= case_when(yrmo == 202011 & vendors_change=="Less" ~ NA_character_,
+                                     TRUE ~ vendors_change),
+           weight_firewood= case_when(yrmo == 202011 & 
+                                        settlement == "kiryandongo" & 
+                                        weight_firewood == 2 ~ 6,
+                                      TRUE ~ weight_firewood)
+    )
+}
+
 # get files names and full paths into a list
 get_files_metadata<-function(folder_path){
   fps<-list()
